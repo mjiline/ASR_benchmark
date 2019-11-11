16ff5be86001dc81425e4bd220a82109ab8bab77
zhilin <misha@epiphan.com>
2019-11-11 17:21:12 -0500

    Adapted to DS6

--- src/run_freespeech5.sh
+++ src/run_freespeech5.sh
@@ -1,19 +1,27 @@
-#!/bin/bash
+#!/bin/bash -x
+
+PPID=$$
+function handler {
+  trap INT
+  kill $PPID
+}
+trap handler INT
+#shopt -s execfail
 
 
-DS_DIR=/home/ubuntu/speech/DS-5
+DS_DIR=/tf/DS6
+
+MNAME=ds-other-2048
+MODEL="${DS_DIR}/models/export-${MNAME}/output_graph.pbmm"
 #MODEL="${DS_DIR}/DeepSpeech/native_client/deepspeech-0.5.1-models/output_graph.pbmm"
-MODEL="${DS_DIR}/DeepSpeech/model-mzh256/output_graph.pbmm"
-#MODEL="${DS_DIR}/DeepSpeech/model-mzh512/output_graph.pbmm"
-#MODEL="${DS_DIR}/DeepSpeech/model-mzh2048/output_graph.pbmm"
 
 
 LMMOD_DIR="${DS_DIR}/DeepSpeech/data"
 export LD_LIBRARY_PATH="${DS_DIR}/tensorflow/bazel-bin/native_client"
 
-exec "${DS_DIR}/DeepSpeech/native_client/deepspeech" \
+#    --alphabet "${LMMOD_DIR}/alphabet.txt"
+${DS_DIR}/DeepSpeech/native_client/deepspeech \
     --model "$MODEL" \
-    --alphabet "${LMMOD_DIR}/alphabet.txt" \
     --lm "${LMMOD_DIR}/lm/lm.binary" \
     --trie "${LMMOD_DIR}/lm/trie" \
     $@
