a7ac846b779be1346ec6a4502c69741bbb366e2d
Misha Jiline <misha@epiphan.com>
2020-01-07 19:25:44 +0000

    Added GOOGLE_APPLICATION_CREDENTIALS

--- src/benchmark.sh
+++ src/benchmark.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+export GOOGLE_APPLICATION_CREDENTIALS="${DIR}/Transcriber-134d5988de26.json"
+
 exec /usr/bin/env python3 -u benchmark.py
